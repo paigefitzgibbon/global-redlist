86d30a600ebe4c11c623f2af5c71d8fb83ef39d1
Rachel Kenny <45524939+rlkenny@users.noreply.github.com>
2019-02-25 16:15:09 -0800

    Update app.R

--- app.R
+++ app.R
@@ -1,51 +1,49 @@
 library(shiny)
 library(tidyverse)
 
-# Define UI for application
+# Define UI for application that draws a histogram
 ui <- fluidPage(
-  
-  # Application title
-  titlePanel("Exploring Population Growth and the Global Distribution of Red List Species"),
-  
-  # Sidebar with a picture of threatened species 
-  sidebarLayout( position = "left",
-    sidebarPanel("Leatherback Sea Turtle"),
-    
-    # Show a summary of the app on main panel
-   navbarPage("Here's the main title!",
-              tabPanel(
-                h1("Summary of the app"),
-                h2("This app will examine the relationship between human
-                   population growth and the global distribution of IUCN Red List species. The goal of this app is to help visualize how many and what class of listed species are located in each country compared to historical and projected population growth."),
-                h3("Our app will utilize three datasets:"),
-                h3("The first dataset contains the number of threatened International Union for the Conservation of Nature (IUCN) Red List species in each country catego- rized by continent (IUCN Red List of Threatened Species 2018)"),
-                h3("The second dataset describes 273 observations of total population (includ- ing both sexes) by region, subregion, and country, annually for 1950-2100 (thousands) (United Nations 2017)."),
-                h3("The third dataset describes 273 observations of average annual rates of population change by region, subregion, and country for 1950-2100 (per- centage) (United Nations 2017).")
-              )
-   ),
-     mainPanel(
-       tabsetPanel(
-         tabPanel("Home Page",
-                  plotOutput("distPlot")) )
-        
-      )
-    )
-  )
-
-
-# Define server logic required to draw a histogram
-#server <- function(input, output) {
    
-   #output$distPlot <- renderPlot({
+   # Application title
+   titlePanel("Old Faithful Geyser Data"),
+   
+   # Sidebar with a slider input for number of bins 
+   sidebarLayout(position = "left",
+      sidebarPanel("Leatherback Sea Turtle",
+         sliderInput("bins",
+                     "Number of bins:",
+                     min = 1,
+                     max = 50,
+                     value = 30)
+      ),
+      
+      # Show a plot of the generated distribution
+      mainPanel("This is the main panel title",
+         plotOutput("distPlot"),
+         tabsetPanel(
+             type = "tab",
+             tabPanel("Distribution",
+                      plotOutput("distPlot")),
+             tabPanel("Scatterplot",
+                      plotOutput("scatterplot"))
+           )
+         )
+      )
+   )
+   
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+   
+   output$distPlot <- renderPlot({
       # generate bins based on input$bins from ui.R
-     # x    <- faithful[, 2] 
-      #bins <- seq(min(x), max(x), length.out = input$bins + 1)
+      x    <- faithful[, 2] 
+      bins <- seq(min(x), max(x), length.out = input$bins + 1)
       
       # draw the histogram with the specified number of bins
-      #hist(x, breaks = bins, col = 'darkgray', border = 'white')
-   #})
-#}
+      hist(x, breaks = bins, col = 'darkgray', border = 'white')
+   })
+}
 
 # Run the application 
-#shinyApp(ui = ui, server = server)
+shinyApp(ui = ui, server = server)
 
