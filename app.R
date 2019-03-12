04ea08ec331c67cf9ea5c84055b578e5850cb92f
Rachel Kenny <rachelkenny@ucsb.edu>
2019-03-11 23:09:24 -0700

    Edited scatterplot to display all countries and then highlight continent so we can compare between continents. Added in instructions on how to use the app. Applied consistent color scheme and added text formatting (size hierarchy, bold, etc).

--- app.R
+++ app.R
@@ -12,6 +12,7 @@ library(RColorBrewer)
 library(plotly)
 library(png)
 library(imager)
+library(wesanderson)
 
 # Read in data
 
@@ -52,7 +53,7 @@ Graph2000 <- ggplot(data = world_df) +
   scale_fill_manual(values = cols) +
   theme_minimal() +
   coord_sf(datum=NA) +
-  labs(x = "", y = "", title = "Distribution of Worldwide Population (Decile)")
+  labs(x = "", y = "", title = " ")
 
 Graph2010<- ggplot(data = world_df) + 
   geom_sf(aes(fill = Total2010), 
@@ -62,7 +63,7 @@ Graph2010<- ggplot(data = world_df) +
   scale_fill_manual(values = cols) +
   theme_minimal() +
   coord_sf(datum=NA) +
-  labs(x = "", y = "", title = "Distribution of Worldwide Population (Decile)")
+  labs(x = "", y = "", title = " ")
 
 Graph2020 <- ggplot(data = world_df) + 
   geom_sf(aes(fill = Total2020), 
@@ -72,7 +73,7 @@ Graph2020 <- ggplot(data = world_df) +
   scale_fill_manual(values = cols) +
   theme_minimal() +
   coord_sf(datum=NA) +
-  labs(x = "", y = "", title = "Distribution of Worldwide Population (Decile)")
+  labs(x = "", y = "", title = " ")
 
 
 Graph2030 <- ggplot(data = world_df) + 
@@ -83,7 +84,7 @@ Graph2030 <- ggplot(data = world_df) +
   scale_fill_manual(values = cols) +
   theme_minimal() +
   coord_sf(datum=NA) +
-  labs(x = "", y = "", title = "Distribution of Worldwide Population (Decile)")
+  labs(x = "", y = "", title = " ")
 
 Graph2040<- ggplot(data = world_df) + 
   geom_sf(aes(fill = Total2040), 
@@ -93,7 +94,7 @@ Graph2040<- ggplot(data = world_df) +
   scale_fill_manual(values = cols) +
   theme_minimal() +
   coord_sf(datum=NA) +
-  labs(x = "", y = "", title = "Distribution of Worldwide Population (Decile)")
+  labs(x = "", y = "", title = " ")
 
 Graph2050<- ggplot(data = world_df) + 
   geom_sf(aes(fill = Total2050), 
@@ -103,7 +104,7 @@ Graph2050<- ggplot(data = world_df) +
   scale_fill_manual(values = cols) +
   theme_minimal() +
   coord_sf(datum=NA) +
-  labs(x = "", y = "", title = "Distribution of Worldwide Population (Decile)")
+  labs(x = "", y = "", title = " ")
 
 
 # User interface 
@@ -111,7 +112,7 @@ ui <- navbarPage("Exploring Population Growth and the Global Distribution of IUC
                  theme = shinytheme("flatly"),
                  
                  # Home page panel               
-                 tabPanel("Summary",
+                 tabPanel("About",
                           
                           fluidPage(
                             sidebarLayout(
@@ -119,18 +120,14 @@ ui <- navbarPage("Exploring Population Growth and the Global Distribution of IUC
                               ),
                               mainPanel(
                                 br(),
-                                h5("This app will examine the relationship between human
-                                   population growth and the global distribution of IUCN Red List species (Critically Endangered, Endangered and Vulnerable). 
-                                   The goal of this app is to help visualize how many and what class of listed species are located in each country compared to historical and projected population growth."),
+                                h3("This app will examine the relationship between human
+                                   population growth and the global distribution of IUCN Red List species (Critically Endangered, Endangered and Vulnerable)."), ("The goal of this app is to help visualize how many and what class of listed species are located in each country compared to historical and projected population growth."),
                                 br(),
                                 br(),
-                                br(),
-                                h5("Our app will utilize three datasets:"),
-                                br(),
+                                strong("Our app will utilize three datasets:"),
                                 h5("1) Number of threatened International Union for the Conservation of Nature (IUCN) Red List species in each country categorized by continent (IUCN Red List of Threatened Species 2018)."),
                                 h5("2) Total population (including both sexes) by region, subregion, and country, annually for 1950-2100 (thousands) (United Nations 2017)."),
                                 h5("3) Average annual rates of population change by region, subregion, and country for 1950-2100 (percentage) (United Nations 2017)."),
-                                br(),
                                 br(),
                                 br(),
                                 h6("Creators: Paige FitzGibbon, Rachel Kenny, and Madison Meltzer")
@@ -140,15 +137,33 @@ ui <- navbarPage("Exploring Population Growth and the Global Distribution of IUC
                           )
                  ),
                  
+                 # Map panel
+                 tabPanel("Population over time",
+                          titlePanel("Distribution of Worldwide Population"),
+                          h5("Move the slider to see how the distribution of global population has changed since 2000 and is projected to change by 2050."),
+                          br(),
+                          sidebarLayout(
+                            sidebarPanel(sliderInput("year", "Year",
+                                                     min = 2000, max = 2050,
+                                                     value = 2000, step = 10)),
+                            mainPanel(
+                              imageOutput(outputId = "map")
+                            )
+                          )
+                          
+                 ),
+                 
                  # Bar graph panel
-                 tabPanel("Bar Graph",
+                 tabPanel("IUCN Listed Species by Country",
                           titlePanel("Number of IUCN Threatened Species by Country"),
+                          h5("Select two countries to compare the number of IUCN listed threatened species in each. It's important to note that the number of listed species can be greatly affected by the availability of data."),
+                          br(),
                           sidebarLayout(
                             sidebarPanel(
                               selectInput("country_1", "Country 1",
                                           choices = c("Algeria","China","Denmark","France","Ghana"),
-                                                     selected = "China",
-                                                     multiple = FALSE),
+                                          selected = "China",
+                                          multiple = FALSE),
                               selectInput("country_2", "Country 2",
                                           choices = c("Algeria","China","Denmark","France","Ghana"),
                                           selected = "Ghana",
@@ -161,26 +176,15 @@ ui <- navbarPage("Exploring Population Growth and the Global Distribution of IUC
                           
                  ),
                  
-                 # Map panel
-                 tabPanel("Map",
-                          titlePanel("Distribution of Worldwide Population"),
-                          sidebarLayout(
-                            sidebarPanel(sliderInput("year", "Year",
-                                                     min = 2000, max = 2050,
-                                                     value = 2000, step = 10)),
-                            mainPanel(
-                              imageOutput(outputId = "map")
-                            )
-                          )
-                          
-                 ),
                  
                  # Scatterplot panel
-                 tabPanel("Scatterplot",
+                 tabPanel("Exploring countries at risk",
                           titlePanel("Countries At Risk"),
+                          h5("First select a continent you want to explore and a species class that you're interested in. Finally select one of three variations (high, low, medium) of population growth rate variant. Hover over each point to see details for each country."),
+                          br(),
                           sidebarLayout(
                             sidebarPanel(
-                                          
+                              
                               #Drop-down menu to choose continent of interest
                               radioButtons("continent",
                                            label = "Select a Continent:",
@@ -204,116 +208,120 @@ ui <- navbarPage("Exploring Population Growth and the Global Distribution of IUC
                               verbatimTextOutput("hover")
                             )
                           )
-                           
-                         )
+                          
+                 )
                  
                  
-)
+                 )
 
 
 
 # Define server logic
 server <- function(input, output) {
-
+  
   scatterplot_filtered <- reactive({
     scatterplot_df1 %>%
-    filter(Continent == input$continent)
+      filter(Continent == input$continent)
   })
-
+  
   bio_df <- reactive({
     bio %>%
-    filter(Country == input$country_1 | Country == input$country_2)
+      filter(Country == input$country_1 | Country == input$country_2)
   })
   
   # Generate plotly scatterplot of requested variables
   output$scatterplot <- renderPlotly({
-
-    plot <- ggplot(data = scatterplot_filtered(), 
-           aes_string(x = input$x, y = input$y)) +
-      geom_point(aes(fill = input$continent, text = paste("Country:", Country)), show.legend = FALSE,
-                 size = 4, alpha = 0.7, color = "dodgerblue2") +
+    
+    
+    
+    plot <- ggplot(data = scatterplot_df1, 
+                   aes_string(x = input$x, y = input$y)) +
+      geom_point(aes(text = paste("Country:", Country)), show.legend = TRUE,
+                 size = 4, alpha = 0.7) +
+      geom_point(data=scatterplot_filtered(), colour="turquoise1", alpha = .7, size=3)+
       labs(x = "Number of IUCN Listed Threatened Species",
-           y = "Rate of Population Increase (2050)") +
+           y = "2050 Projected Rate of Population Increase (%)") +
       scale_fill_brewer(palette="Paired") +
       theme(panel.grid.major = element_line(color = gray(0.5), 
                                             linetype = "blank", 
                                             size = 0.5), 
             panel.background = element_rect(fill = "white"),
-            axis.title = element_text(face="bold"), title = element_text(face="bold"))
+            axis.title = element_text(face="bold"), title = element_text(face="bold"))+
+      theme_classic()
     
     require(plotly)
     ggplotly(plot, tooltip = c("text", "x", "y"))
     
   })
-
-# Generate  bar graph of requested variables
-output$bargraph <- renderPlot({
-  ggplot(data = bio_df(), 
-         aes(x = bio_df()$species_type, y = bio_df()$thr_count, fill = bio_df()$Country)) +
-    geom_bar(stat="identity", position="dodge") +
-    labs(x = "Species Type", y = "Count") +
-    theme(panel.grid.major = element_line(color = gray(0.5), 
-          linetype = "blank", size = 0.5), 
-          panel.background = element_rect(fill = "white"))+
-    theme_classic()+
-    theme(axis.title = element_text(face="bold"), title = element_text(face="bold"))+
-    scale_fill_brewer(palette="Paired")+
-    scale_x_discrete(expand=c(0.15,0))+
-    scale_y_continuous(expand=c(0,0))+
-    labs(fill="Country")
+  
+  # Generate  bar graph of requested variables
+  output$bargraph <- renderPlot({
+    ggplot(data = bio_df(), 
+           aes(x = bio_df()$species_type, y = bio_df()$thr_count, fill = bio_df()$Country)) +
+      geom_bar(stat="identity", position="dodge") +
+      labs(x = "Species Type", y = "Count") +
+      theme(panel.grid.major = element_line(color = gray(0.5), 
+                                            linetype = "blank", size = 0.5), 
+            panel.background = element_rect(fill = "white"))+
+      theme_classic()+
+      theme(axis.title = element_text(face="bold"), title = element_text(face="bold"))+
+      scale_fill_manual(values=c("turquoise3","turquoise4"))+
+      scale_x_discrete(expand=c(0.15,0))+
+      scale_y_continuous(expand=c(0,0))+
+      labs(fill="Country")
   })
-
-output$map <- renderImage({
-  if (input$year == 2000) 
-  {
-    return(list(
-      src = "plot2000.png",
-      contentType = "image/png",
-      alt = "2000"
-    ))
+  
+  output$map <- renderImage({
+    if (input$year == 2000) 
+    {
+      return(list(
+        src = "plot2000.png",
+        contentType = "image/png",
+        alt = "2000"
+      ))
+    } 
+    if (input$year == 2010)
+    {
+      return(list(
+        src = "plot2010.png",
+        contentType = "image/png",
+        alt = "2010"
+      ))
+    } 
+    if (input$year == 2020)
+    {
+      return(list(
+        src = "plot2020.png",
+        contentType = "image/png",
+        alt = "2020"
+      ))
+    } 
+    if (input$year == 2030)
+    {
+      return(list(
+        src = "plot2030.png",
+        contentType = "image/png",
+        alt = "2030"
+      ))
+    } 
+    if (input$year == 2040)
+    {
+      return(list(
+        src = "plot2040.png",
+        contentType = "image/png",
+        alt = "2040"
+      ))
+    } 
+    else if(input$year == 2050)
+    {
+      return(list(
+        src = "plot2050.png",
+        contentType = "image/png",
+        alt = "2050"
+      ))
+    } 
   } 
-  if (input$year == 2010)
-  {
-    return(list(
-      src = "plot2010.png",
-      contentType = "image/png",
-      alt = "2010"
-    ))
-  } 
-  if (input$year == 2020)
-  {
-    return(list(
-      src = "plot2020.png",
-      contentType = "image/png",
-      alt = "2020"
-    ))
-  } 
-  if (input$year == 2030)
-  {
-    return(list(
-      src = "plot2030.png",
-      contentType = "image/png",
-      alt = "2030"
-    ))
-  } 
-  if (input$year == 2040)
-  {
-    return(list(
-      src = "plot2040.png",
-      contentType = "image/png",
-      alt = "2040"
-    ))
-  } 
-  else if(input$year == 2050)
-  {
-    return(list(
-      src = "plot2050.png",
-      contentType = "image/png",
-      alt = "2050"
-    ))
-  } 
-  } 
-, deleteFile = FALSE)
+  , deleteFile = FALSE)
   
 }
 
